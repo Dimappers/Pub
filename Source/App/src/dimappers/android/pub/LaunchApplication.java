da705e82b35815ec50d980c8c38b7348a458997c
thk123 <thekileyenator@gmail.com>
2011-12-17 16:27:10 +0000

    Inital commit

---
+++ Source/App/src/dimappers/android/pub/LaunchApplication.java
@@ -0,0 +1,13 @@
+package dimappers.android.pub;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LaunchApplication extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
---
+++ Source/Server/src/dimappers/android/server/Server.java
@@ -0,0 +1,14 @@
+package dimappers.android.server;
+
+public class Server {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Server running");
+		
+		System.out.println("Server closing...");
+	}
+
+}
