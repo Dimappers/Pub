b4533521aaeea1bf3e58c4f5240e33c56b31d397
kbarrett <x_kimmi_13_x@hotmail.co.uk>
2012-01-15 20:40:42 +0000

    some additions to screens

--- Source/App/src/dimappers/android/pub/LaunchApplication.java
+++ Source/App/src/dimappers/android/pub/LaunchApplication.java
@@ -2,12 +2,25 @@ package dimappers.android.pub;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class LaunchApplication extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+    	super.onCreate(savedInstanceState);
+    	setContentView(R.layout.home);
+    	//LinearLayout organise = (LinearLayout)findViewById(R.id.organise_screen);
+    	//organise.removeView(organise);
+    
+    	/*Button button_organise = (Button)findViewById(R.id.organise_button);
+    	button_organise.setOnClickListener(new View.OnClickListener() {
+    		public void onClick(View v) {
+    			LinearLayout organise = (LinearLayout)findViewById(R.id.organise_screen);
+    			organise.addView(getLayoutInflater().inflate(R.layout.organise, null));
+    		}
+    	});*/
     }
 }
