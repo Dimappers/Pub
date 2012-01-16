bad479055c61f97726a1a0cee9ca7e2ffe89af81
kbarrett <x_kimmi_13_x@hotmail.co.uk>
2012-01-16 21:26:53 +0000

    some additions to screens
    organise button linking to new screen

--- Source/App/src/dimappers/android/pub/LaunchApplication.java
+++ Source/App/src/dimappers/android/pub/LaunchApplication.java
@@ -1,26 +1,46 @@
 package dimappers.android.pub;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.SyncStateContract.Constants;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
-public class LaunchApplication extends Activity {
+public class LaunchApplication extends Activity implements OnClickListener{
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
     	super.onCreate(savedInstanceState);
-    	setContentView(R.layout.home);
-    	//LinearLayout organise = (LinearLayout)findViewById(R.id.organise_screen);
-    	//organise.removeView(organise);
-    
-    	/*Button button_organise = (Button)findViewById(R.id.organise_button);
-    	button_organise.setOnClickListener(new View.OnClickListener() {
-    		public void onClick(View v) {
-    			LinearLayout organise = (LinearLayout)findViewById(R.id.organise_screen);
-    			organise.addView(getLayoutInflater().inflate(R.layout.organise, null));
-    		}
-    	});*/
+    	setContentView(R.layout.main);
+    	Toast.makeText(getApplicationContext(), "test", Toast.LENGTH_LONG).show();
+    	
+    	Button button_organise = (Button)findViewById(R.id.organise_button);
+    	button_organise.setOnClickListener(this);
+    	
+    	Button button_invites = (Button)findViewById(R.id.invites_button);
+    	button_organise.setOnClickListener(this);
+    	
+    }
+    public void onClick(View v)
+    {
+    	Intent i;
+		
+		switch (v.getId()){
+		case R.id.organise_button : 
+
+			i = new Intent(this, Organise.class);
+	    	Toast.makeText(getApplicationContext(), "trying to", Toast.LENGTH_LONG).show();
+			startActivityForResult(i, 0);
+			break;
+			
+		case R.id.invites_button :
+			finish();
+			break;
+		}
     }
 }
