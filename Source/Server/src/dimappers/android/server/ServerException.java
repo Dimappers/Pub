88781de10eda97a43ab2c61760221a8cb8e70071
thk123 <thekileyenator@gmail.com>
2012-01-24 14:50:25 +0000

    Working on error handling on the server Fixing bug in ServerTest - UpdateMessage does not pass correct ID but test still passes

---
+++ Source/Server/src/dimappers/android/server/ServerException.java
@@ -0,0 +1,26 @@
+package dimappers.android.server;
+
+public class ServerException extends Exception 
+{
+	private ExceptionType exceptionType;
+	
+	public ServerException(ExceptionType t)
+	{
+		super();
+		
+		exceptionType = t;
+	}
+	
+	public ExceptionType GetExceptionType()
+	{
+		return exceptionType;
+	}
+}
+
+enum ExceptionType
+{
+	ReadingObjectNewEvent,
+	CastingObjectNewEvent,
+	SendingErrorBack,
+	UnknownError
+}
