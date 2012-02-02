e222b2528c33f59ca336c2cd5c45100f7c9f71a3
thk123 <thekileyenator@gmail.com>
2012-02-02 15:43:30 +0000

    Added constants properly

---
+++ Source/PubData/src/dimappers/android/PubData/Constants.java
@@ -0,0 +1,15 @@
+package dimappers.android.PubData;
+
+public final class Constants {
+	//Common bundle tags
+	public static final String CurrentWorkingEvent = "event";
+	public static final String CurrentUserFacebookUser = "facebookId";
+	
+	//Error constants
+	public static final int MissingDataInBundle = -12;
+	
+	//Server stuff
+	public static final String ServerIp = "127.0.0.1";
+	public static final int Port = 2085;
+	
+}
