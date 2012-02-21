d4b4c4494bbf9cc2d21b6e33dc03ccf310ac52bb
thk123 <thekileyenator@gmail.com>
2012-02-21 15:01:58 +0000

    Working on data receiver issuing notifications

--- Source/App/src/dimappers/android/pub/DataReceiver.java
+++ Source/App/src/dimappers/android/pub/DataReceiver.java
@@ -1,6 +1,8 @@
 package dimappers.android.pub;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -11,12 +13,19 @@ import org.jdom.JDOMException;
 import org.jdom.input.SAXBuilder;
 import org.jdom.output.XMLOutputter;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
 import android.util.Log;
 
 import dimappers.android.PubData.Constants;
 import dimappers.android.PubData.MessageType;
 import dimappers.android.PubData.PubEvent;
 import dimappers.android.PubData.RefreshData;
+import dimappers.android.PubData.User;
 
 public class DataReceiver
 {
@@ -91,11 +100,44 @@ public class DataReceiver
 			Element eventsRoot = returnDocument.getRootElement().getChild("PubEvents");
 			
 			List<Element> eventsElements = eventsRoot.getChildren(PubEvent.class.getSimpleName());
+			ArrayList<PubEvent> newEvents = new ArrayList<PubEvent>();
 			for(Element eventElement : eventsElements)
 			{
 				PubEvent newEvent = new PubEvent(eventElement);
 				service.getDataStore().AddNewInvitedEvent(newEvent);
+				newEvents.add(newEvent);
 			}*/
+			
+			//Not working or maybe is cound't debug
+			ArrayList<PubEvent> newEvents; 
+			newEvents = new ArrayList<PubEvent>();
+			newEvents.add(new PubEvent(Calendar.getInstance(), new User(124)));
+			
+			if(newEvents.size() > 0)
+			{
+				NotificationManager nManager = (NotificationManager)service.getSystemService(Context.NOTIFICATION_SERVICE);
+				if(newEvents.size() == 1)
+				{
+					Notification newNotification = new Notification(R.drawable.icon, "New pub event", System.currentTimeMillis());
+					Context context = service.getApplicationContext();
+					Intent notificationIntent = new Intent(context, UserInvites.class);
+					Bundle b = new Bundle();
+					b.putSerializable(Constants.CurrentWorkingEvent, newEvents.get(0));
+					b.putSerializable(Constants.CurrentFacebookUser, service.user);
+					notificationIntent.putExtras(b);
+					PendingIntent contentIntent = PendingIntent.getActivity(context, 0, notificationIntent, 0);
+					
+					newNotification.setLatestEventInfo(context, "New Pub Event", newEvents.get(0).toString(), contentIntent);
+					
+					nManager.notify(1, newNotification);
+				}
+				else
+				{
+					
+				}
+			}
+			
+			
 		}
 		
 	}
